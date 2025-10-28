0bcf84d432c0cdf40933c4ee7acc5338b3437fd9
Alex <alejandro.sa.barroso@gmail.com>
2025-10-28 17:20:24 +0100

    Ejercicios 04 y 07 Tema 4

---
+++ Tema 4/Ejercicio04/src/ejercicio04/Ordenacion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio04;
+
+/**
+ *
+ * @author Alex
+ */
+public class Ordenacion {
+     public static float Ordenar(float num1, float num2,float num3){
+    
+          if ((num1 < num2) && (num1 < num3)) {
+            return num1;
+        }else if ((num2 < num1) && (num2 < num3)){
+           return num2;
+        }else 
+           return num3;
+        
+    }
+}
