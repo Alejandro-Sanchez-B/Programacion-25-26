c0b18fc3176fa35aaf7a62e9feedf2f85808713d
Alex Sánchez <alejandro.sa.barroso@gmail.com>
2025-11-11 15:25:15 +0100

    Ejercicio Bicicleta

---
+++ Tema 5/Ejercicio01/src/ejercicio01/Coche.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio01;
+
+/**
+ *
+ * @author alumno
+ */
+public class Coche {
+   
+   //Atributos. Nunca se inicializan aqui
+   private String marca;
+   private String modelo;
+   private String color;
+   private int velocidad;
+   private boolean motorEncendido;
+
+
+    public Coche (){
+        this.marca = "";
+        this.modelo = "";
+        this.color = "";
+        this.velocidad = 0;
+        this.motorEncendido = false;
+        
+        
+        
+
+    }
+    
+    public void setMarca(String marca){
+        
+    } 
+}
